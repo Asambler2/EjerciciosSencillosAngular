f1905228fcd3fbaa6049b90b8ce8c0b30277994f
Asambler2 <chema508@hotmail.com>
2024-04-22 13:02:00 +0200

    Ej3 hecho

--- Ej3FormularioColores/src/app/app.component.ts
+++ Ej3FormularioColores/src/app/app.component.ts
@@ -12,14 +12,17 @@ export class AppComponent {
 
   colorRojo(): void {
     this.rojo = Math.floor(Math.random() * 255);
-    document.getElementById('rojo')?.setAttribute('style', `background-color: rgb(${this.rojo.toString() }, 0, 0)`);
+    document.getElementById('rojo')?.setAttribute('style', `background-color: rgb(${this.rojo.toString()}, 0, 0)`);
+    document.getElementsByTagName('body')[0]?.setAttribute('style', `background-color: rgb(${this.rojo.toString()}, ${this.verde.toString()}, ${this.azul.toString()})`);
   }
   colorVerde(): void {
     this.verde = Math.floor(Math.random() * 255);
     document.getElementById('verde')?.setAttribute('style', `background-color: rgb(0, ${this.verde.toString()}, 0)`);
+    document.getElementsByTagName('body')[0].setAttribute('style', `background-color: rgb(${this.rojo.toString()}, ${this.verde.toString()}, ${this.azul.toString()})`);
   }
   colorAzul(): void {
     this.azul = Math.floor(Math.random() * 255);
     document.getElementById('azul')?.setAttribute('style', `background-color: rgb(0, 0, ${this.azul.toString()})`);
+    document.getElementsByTagName('body')[0].setAttribute('style', `background-color: rgb(${this.rojo.toString()}, ${this.verde.toString()}, ${this.azul.toString()})`);
   }
 }
